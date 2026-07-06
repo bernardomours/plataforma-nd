9a02a5db00411753b32ab31efb2c0248e8f89d71
Bernardo <bernardo.araujo1612@gmail.com>
2026-07-06 09:41:27 -0300

    ajustando a logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
 
         <title>Plataforma ND</title>
         <link rel="preconnect" href="https://fonts.bunny.net">
+        <link rel="icon" href="{{ asset('images/logo-site.png') }}" type="image/png">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
